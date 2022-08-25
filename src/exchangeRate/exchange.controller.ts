1a1e75fe6681b0bdae899e5efa7177607ac3b4b7
Theodory <theodoryfg@gmail.com>
2022-08-25 10:23:58 +0300

    Add exchange controller methods

--- src/exchangeRate/exchange.controller.ts
+++ src/exchangeRate/exchange.controller.ts
@@ -23,5 +23,44 @@ import { ApiTags } from '@nestjs/swagger'
 export class ExchangeRateController {
   constructor(private readonly ExchangeRateService: ExchangeRateService) {}
 
+  @Post()
+  createRates(@Body() rate: CreateExchangeRateDto): any {
+    return this.ExchangeRateService.createRate(rate);
+  }
 
+  @Get()
+  getRates(
+    @Query('q') keyword?: string,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit?: number,
+    @Query('skip', new DefaultValuePipe(0), ParseIntPipe) skip?: number,
+  ): any {
+    return this.ExchangeRateService.getRates(keyword, skip, limit);
+  }
+
+  @Get('/convert')
+   exhange(@Query() q: ConversionDto): any{
+    return  this.ExchangeRateService.exchange(q);
+  }
+
+  @Get('/rate')
+  getRate(
+    @Query() q: RateExchangeDto
+    )
+    : any {
+
+    return this.ExchangeRateService.getRate(q);
+  }
+
+  @Patch(':id')
+  updateRate(
+    @Param('id') id: string,
+    @Body() rate: UpdateExchangeRateDto,
+  ): any {
+    return this.ExchangeRateService.updateRate(id, rate);
+  }
+
+  @Delete(':id')
+  deleteRate(@Param('id') id: string): any {
+    return this.ExchangeRateService.deleteRate(id);
+  }
 }
